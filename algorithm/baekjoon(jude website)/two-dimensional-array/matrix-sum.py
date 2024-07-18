d1114689679249748f756133ff6b1ab5c60d2fe4
Yea Sung <98846057+yeasung240@users.noreply.github.com>
2024-07-18 14:49:07 +0200

    Create matrix-sum.py

---
+++ algorithm/baekjoon(jude website)/two-dimensional-array/matrix-sum.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+
+A = [list(map(int,input().split())) for i in range(N)]
+B = [list(map(int,input().split())) for i in range(N)]
+
+
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end = ' ')
+    print()
